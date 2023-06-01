9e5e21cc903f03defc77c334025f0ac3e49ec3ec
yoonyoungheon <tbvjgjsl9@naver.com>
2023-06-02 01:37:01 +0900

    yh

---
+++ DGU_project/src/Class.java
@@ -0,0 +1,18 @@
+
+	class Class {
+		String classname;
+		String professor;
+		String classroom;
+		String time;
+		String mutualsub;
+		String tool;
+		
+		public Class(String classname, String professor, String classroom, String time, String mutualsub, String tool) {
+			this.classname = classname;
+			this.professor = professor;
+			this.classroom = classroom;
+			this.time = time;
+			this.mutualsub = mutualsub;
+			this.tool = tool;
+		}
+	}
--- DGU_project/src/ReadCSV.java
+++ DGU_project/src/ReadCSV.java
@@ -20,29 +20,28 @@ public class ReadCSV {
 				
 
 	        String line = br.readLine();
-	        //������ �л� ��ü ����
+
 	        if (line == null) {
 	        	sl.add(new Student(name.get(0),subject,grade));//마지막 학생 저장
 	        	break;  
 	        }
 
-	        String[] result = line.split(",");  //3���̴�, result�� 1�� 3��(name, subject, grade)
+	        String[] result = line.split(",");  
 	        
 	        name.add(result[0]);subject.add(result[1]);grade.add(result[2]);
-	        
-	        //�л� ��ü ����
-	        if (i>=1) {//2�� �̻��� ������ ����Ǿ� ���� ��,
-	        	if (!name.get(i).equals(name.get(i-1))) {//i�� ���� �ֱ� index
-	        		sl.add(new Student(name.get(i-1),new ArrayList<>(subject.subList(0, i)),new ArrayList<>(grade.subList(0, i))));//index i�����ϰ�� �� name ����
-//i 이하는 다 같은 학생의 것이기에 학생 추가 위에서 했으면,i만 남기고 다 삭제 -> i=0으로
+
+	        if (i>=1) {
+	        	if (!name.get(i).equals(name.get(i-1))) {
+	        		sl.add(new Student(name.get(i-1),new ArrayList<>(subject.subList(0, i)),new ArrayList<>(grade.subList(0, i))));
+
 	        		for (int j = 0; j <= i-1; j++) {
-	        		    name.remove(0); // 인덱스 0의 요소를 삭제
+	        		    name.remove(0); 
 	        		}
 	        		for (int j = 0; j <= i-1; j++) {
-	        		    subject.remove(0); // 인덱스 0의 요소를 삭제
+	        		    subject.remove(0); 
 	        		}
 	        		for (int j = 0; j <= i-1; j++) {
-	        		    grade.remove(0); // 인덱스 0의 요소를 삭제
+	        		    grade.remove(0); 
 	        		}
 	        		i=0;
 	        	}
